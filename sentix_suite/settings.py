b8c24b1d09b85759ffcd190bde65000cfb981a53
Poornima Shaja <poornimanadar1234@gmail.com>
2026-03-31 01:40:18 +0530

    updated cs50w final

--- sentix_suite/settings.py
+++ sentix_suite/settings.py
@@ -78,12 +78,19 @@ WSGI_APPLICATION = 'sentix_suite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default='postgresql://postgres:postgres@localhost:5432/mysite',
+#         conn_max_age=600
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 AUTH_USER_MODEL = 'processor.User'
 # Password validation
